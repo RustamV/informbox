7d7a414e3b3d8f132365c7b16df4db923688a85f
emertarion <rvrd1996@gmail.com>
2020-09-02 21:04:30 +0500

    first commit

---
+++ build/precache-manifest.daa2678d2553d8cf74bfbc54e2cb3a05.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ab91ded80407de04a8bb7d06ed04491",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64cfe2a91f295754aaec",
+    "url": "/static/css/main.5c2d43d0.chunk.css"
+  },
+  {
+    "revision": "0fb3f36656e7c5909107",
+    "url": "/static/js/2.f18587cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f18587cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64cfe2a91f295754aaec",
+    "url": "/static/js/main.a0282d14.chunk.js"
+  },
+  {
+    "revision": "acc64413b3be90903838",
+    "url": "/static/js/runtime-main.f3c193b7.js"
+  },
+  {
+    "revision": "71fc7af90fa9796fdfc18ce2b82be299",
+    "url": "/static/media/SegoeUI-Bold.71fc7af9.woff"
+  },
+  {
+    "revision": "ee10411f11ca61338f5bcaebefbdf700",
+    "url": "/static/media/SegoeUI.ee10411f.woff"
+  }
+]);
